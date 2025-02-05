94d93a3a1468e583f03231a51f2de0c6a714a52b
XavOli <xavoli2099@gmail.com>
2025-02-05 13:20:10 -0500

    Mobile hamburger menu using font awesome icons. Added responsiveness to screen size (without refreshing page) and closes when path is changed.

--- components/NavBar/AppNav.js
+++ components/NavBar/AppNav.js
@@ -2,17 +2,44 @@
 
 import NavBtn from '@/components/NavBar/NavBtn';
 import HomeBtn from '@/components/NavBar/HomeBtn';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { usePathname } from 'next/navigation';
 
 export default function AppNav() {
-  const [showNav, setShowNav] = useState(true);
+  const [showNav, setShowNav] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (window.innerWidth < 768) setShowNav(false);
+  }, [pathname]);
+
+  useEffect(function () {
+    function handleResize() {
+      const screenWidth = window.innerWidth;
+      if (screenWidth >= 768) {
+        setShowNav(true);
+      } else {
+        setShowNav(false);
+      }
+    }
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <div className=" w-full  flex items-center justify-center border-b-2 border-gray-800 backdrop-blur-md bg-gray-800 bg-opacity-50">
       <div className="w-full py-2 px-10 flex flex-col md:flex-row justify-between">
         <div className="w-full md:w-fit flex justify-between">
           <HomeBtn />
           <button onClick={() => setShowNav(!showNav)} className="md:hidden">
-            X
+            {showNav ? (
+              <FontAwesomeIcon icon={faX} size="2x" />
+            ) : (
+              <FontAwesomeIcon icon={faBars} size="2x" />
+            )}
           </button>
         </div>
         {showNav && (
